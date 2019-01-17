b8d95c6bdffd3921e1654d09ac23106c187fd47a
WHS241 <hansen.wu@vanderbilt.edu>
2019-01-17 09:39:13 -0600

    Import files

---
+++ Scripts/DistanceView.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DistanceView : MonoBehaviour {
+    public GameObject closeMarker;
+    public GameObject farMarker;
+    public GameObject indicator;
+
+    public void requestDistanceUse(bool farDist)
+    {
+        closeMarker.SetActive(!farDist);
+        farMarker.SetActive(farDist);
+        indicator.GetComponent<Indicators>().ledge = farDist ? farMarker : closeMarker;
+    }
+}
